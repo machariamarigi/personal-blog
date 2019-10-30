615319225a999d7f576a86b412ce1e4d4596316c
machariamarigi <machariamarigi@gmail.com>
2019-10-30 18:31:51 +0300

    create post model

---
+++ src/app/post.model.ts
@@ -0,0 +1,14 @@
+export interface Post {
+  id: number;
+  title: {
+    rendered: string;
+  };
+  content: {
+    rendered: string;
+  };
+  excerpt: {
+    rendered: string;
+  };
+  date: Date;
+  slug: string;
+}
