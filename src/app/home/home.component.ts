6c6d261bba47cb5585598bca3ab9c5a69b4e0e8a
machariamarigi <machariamarigi@gmail.com>
2019-10-30 18:34:32 +0300

    render all posts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { WordpressService } from '../core/wordpress.service';
+
 @Component({
   selector: 'pb-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  posts$ = this.wordpressService.getPosts();
 
-  constructor() { }
+  constructor(private wordpressService: WordpressService) { 
+    console.log(this.posts$);
+  }
 
   ngOnInit() {
   }
