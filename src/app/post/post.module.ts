fecc56efe6f6facdaa58039354c661c6a51510c8
machariamarigi <machariamarigi@gmail.com>
2019-10-30 18:47:38 +0300

    generate post component

--- src/app/post/post.module.ts
+++ src/app/post/post.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PostRoutingModule } from './post-routing.module';
+import { PostComponent } from './post.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PostComponent],
   imports: [
     CommonModule,
     PostRoutingModule
