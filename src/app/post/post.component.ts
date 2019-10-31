2f87b05d19333cef70e23e9084c827bd40d87ed5
machariamarigi <machariamarigi@gmail.com>
2019-10-31 21:06:25 +0300

    route to an individual post

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { WordpressService } from '../core/wordpress.service';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'pb-post',
@@ -6,10 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent implements OnInit {
+  post: Post;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private wordpressService: WordpressService
+  ) {
+    this.wordpressService.post$.subscribe(data => this.post = data);
+  }
 
   ngOnInit() {
+    this.route.params.subscribe(params => this.wordpressService.getPost(params.id));
   }
 
 }
