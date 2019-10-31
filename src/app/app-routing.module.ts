ab559251c888185224d92806b799a4c3a1f9db5e
machariamarigi <machariamarigi@gmail.com>
2019-10-31 21:05:47 +0300

    lazy-load the post module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,12 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  {
+    path: 'post/:id/:slug',
+    loadChildren: () => import('./post/post.module').then(
+      m => m.PostModule
+    )
+  },
   { path: '**', component: HomeComponent },
 ];
 
