c5d9199acb53c903acc1a1b6c0223ef20984506d
machariamarigi <machariamarigi@gmail.com>
2019-11-08 03:28:09 +0300

    implement posts animations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 import { WordpressService } from '../core/wordpress.service';
+import { postsAnimation } from '../../animations';
 
 @Component({
   selector: 'pb-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    postsAnimation
+  ]
 })
 export class HomeComponent implements OnInit {
   posts$ = this.wordpressService.getPosts();
 
-  constructor(private wordpressService: WordpressService) { 
+  constructor(private wordpressService: WordpressService) {
     console.log(this.posts$);
   }
 
